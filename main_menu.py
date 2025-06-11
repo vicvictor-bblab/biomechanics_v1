0a03617ec150e15e03d43c6dbc0064fd6b3086b1
vicvictor-bblab <aoki.51.tatsu66@gmail.com>
2025-06-12 08:50:33 +0900

    Add main menu script

---
+++ main_menu.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import messagebox
+
+from bio_graph_app import BioGraphApp
+import integrate_files
+
+
+def main():
+    # Create a temporary root window for the dialog
+    root = tk.Tk()
+    root.withdraw()
+
+    answer = messagebox.askquestion(
+        "Select Mode",
+        "Do you want to integrate files?\n\nYes: integrate files\nNo: launch GUI",
+    )
+    root.destroy()
+
+    if answer == 'yes':
+        integrate_files.integrate_files()
+    else:
+        gui_root = tk.Tk()
+        app = BioGraphApp(gui_root)
+        gui_root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
